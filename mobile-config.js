0ff2aba21bba22b4caec1f3c6444741248e40b12
Jonathan <jonathan.loureiro.m@gmail.com>
2017-11-04 18:04:44 -0300

    arrumando versões

---
+++ mobile-config.js
@@ -0,0 +1,28 @@
+App.info({
+  /*id: 'com.example.matt.uber',*/
+  version: "0.0.1",
+  name: 'Ligeirão UFMT',
+  description: 'Protótipo',
+  author: 'lpds_2017_2',
+  buildNumber: 101,
+  /*email: 'contact@example.com',
+  website: 'http://example.com'*/
+});
+
+App.setPreference('Orientation', 'portrait');
+
+/*App.icons({
+  'android_mdpi': 'public/icon/48.png',
+  'android_hdpi': 'public/icon/72.png',
+  'android_xhdpi': 'public/icon/96.png',
+  'android_xhdpi': 'public/icon/144.png',
+  'android_xxxhdpi': 'public/icon/192.png'
+});
+
+App.launchScreens({
+  'android_mdpi_portrait': 'public/lauchscreen/480x800.png',
+  'android_hdpi_portrait': 'public/lauchscreen/480x800.png',
+  'android_xhdpi_portrait': 'public/lauchscreen/720x1280.png',
+  'android_xxhdpi_portrait': 'public/lauchscreen/960x1600.png',
+  'android_xxxhdpi_portrait': 'public/lauchscreen/1280x1920.png'
+});*/

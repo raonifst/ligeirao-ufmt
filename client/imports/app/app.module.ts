9ccd465e56b6d031172edc66bb8c7863e9f5da07
Jonathan <jonathan.loureiro.m@gmail.com>
2017-11-05 03:29:09 -0300

    adding material design

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   declarations: [
     AppComponent,

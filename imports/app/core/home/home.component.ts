43bc79b9a6d749ae3a9c3629081d9511ce476c77
duarte2025 <william.2025@hotmail.com>
2018-02-13 19:25:32 -0300

    OnClick event

--- imports/app/core/home/home.component.ts
+++ imports/app/core/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material';
 
 import { MapService } from '../../shared/map.service';
@@ -8,18 +8,38 @@ import { MapService } from '../../shared/map.service';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
 
+  //pointX = [229, 213, 69, 264];
+  //pointY = [94, 170, 325, 536];
+
   constructor(
     private mapService: MapService
-  ) { }
+  ) {
+  }
+
+  private ngOnInit() {
+
+  }
+
+  private transLinear(resolution) {
+
+  }
 
   private onClickMap(event) {
     //this.trigger.openMenu();
     //this.mapService.myFunction();
-    console.log(event.offsetX+" "+event.offsetY);
+    let fatorW: number = screen.width/412;
+    let fatorH: number = screen.height/732;
+    let pointX = [];
+    let pointY = [];
+    pointX.push(Math.floor(229*fatorW));
+    pointY.push(Math.floor(94*fatorH));
+    console.log(pointX[0], pointY[0]);
+    console.log(event.offsetX+" "+event.offsetY+", "+screen.width+" "+screen.height);
   }
+
 
 }
